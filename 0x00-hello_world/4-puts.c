ffc25052aa9b54e18e8aa1d8533a6d24d1c8f26f
elyas haile <eloyee7@gmail.com>
2022-04-07 15:03:40 -0700

    Hello, putss

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
-int main ()
+/**
+ * main - checking the strings
+ * @void:no input
+ *
+ * Description: adding '' infront of the string
+ * section header: return zero
+ * Return: return the given inpt
+ */
+int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
-}	
+	return (0);
+}
