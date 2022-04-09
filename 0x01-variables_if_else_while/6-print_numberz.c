8b539089e95df28dcfa9d4153e2682af8dcd1f31
elyas haile <eloyee7@gmail.com>
2022-04-09 05:49:27 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - starting with zero
+ * @void: take input from the function
+ * header: return from zero to ten
+ * Return: return zero
+ */
+
+int main(void)
+{
+int num = 0;
+while (num < 10)
+{
+putchar(num + '0');
+num = num + 1;
+}
+return (0);
+}
