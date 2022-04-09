b05971046f668cf8bff5f42fdb1b3b3aeca451e4
elyas haile <eloyee7@gmail.com>
2022-04-09 05:11:02 -0700

    hen I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - starting from a and A
+ * @void: got the Input from the loop
+ * header: return from a to z on both case
+ * Return: return zero
+ */
+int main(void)
+{
+char ch = 'a';
+char CH = 'A';
+while (ch <= 'z')
+{
+if	(ch != 'e')
+if      (ch != 'q')
+putchar(ch);
+ch = ch + 1;
+}
+putchar('\n');
+return (0);
+}
