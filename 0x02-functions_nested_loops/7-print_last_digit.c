9da98dbd10898faf2703a172196d738426a62e6f
elyas haile <eloyee7@gmail.com>
2022-04-13 04:14:24 -0700

    last 777

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * main - looking for num
- * @print_last_digit: print last digit of a number
+ * print_last_digit - looking for num
+ * @n: print last digit of a number
  *
- * Return zero
+ * Return: zero
  */
 
 int print_last_digit(int n)
 {
 int last_digit = n % 10;
 _putchar(last_digit + '0');
-return (0);
+return (last_digit);
 }
