b183b358f942757f31fcbd1978fba1ed04b129c4
elyas haile <eloyee7@gmail.com>
2022-04-13 07:00:45 -0700

    n+m

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - two intiger argument
+ * @n: added variables
+ * @m: added variables
+ *
+ * Return: return addition
+ */
+int add(int n, int m)
+{
+return (n + m);
+}
