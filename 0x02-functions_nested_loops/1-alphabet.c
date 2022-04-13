a6db12e651c4b5c66dcf096565e4af657a8257d3
elyas haile <eloyee7@gmail.com>
2022-04-12 17:55:30 -0700

    abc

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,19 @@
 #include "main.h"
+
 /**
- * main -c printig alphabet
- * @void: start from a
- * header: return from a to z
+ * main - initialize from a
+ * @void: forming loop
+ * description: using customized header
+ * header: return from a to z in lower case
  * Return: return zero
  */
 void print_alphabet(void)
 {
 char i = 'a';
 while (i <= 'z')
-
+{
 _putchar(i);
 i = i + 1;
-
+}
 _putchar('\n');
 }
