a109c20eb0839f0b63a2309b98edef792ce316f7
elyas haile <eloyee7@gmail.com>
2022-04-12 19:31:43 -0700

    isalphA777

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,13 +7,9 @@
  */
 int _islower(int c)
 {
-if (c >= 'a' && c <= 'z')
-if (c >= 'A' && c <= 'Z')
-{
+if ((c >= 'a' && c <= 'z') ||
+(c >= 'A' && c <= 'Z'))
 return (1);
-}
 else
-{
 return (0);
-}
 }
