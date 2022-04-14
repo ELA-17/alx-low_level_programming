2beae70f682f5c6efd4e9acae35c91d6c79a1f07
ELA-17 <101507765+ELA-17@users.noreply.github.com>
2022-04-14 17:27:11 +0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,23 @@
 #include "main.h"
 
 /**
- * print_most_numbers - print digit except 2 and 4
+ * print_most_numbers -  checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
  */
 void print_most_numbers(void)
 {
-int n;
-for (n = 0; n <= 9; n++)
-{
-if ((n == 2) || ((n == 4)))
-continue;
-else
-_putchar(n + '0');
-}
-_putchar(\n);
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		if (c != 50)
+		{
+			if (c != 52)
+			{
+				_putchar(c);
+			}
+		}
+	}
+	_putchar('\n');
 }
