6f688dc421e4e829cb2ba61f4bf4fe60e8399bfe
elyas haile <eloyee7@gmail.com>
2022-04-13 19:56:47 -0700

    multiple

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main -c list natural number multiple
+ *
+ * Return: return zero
+ */
+int main(void)
+{
+int i, sum = 0;
+
+for (i = 0; i < 1024; i++)
+{
+if ((i % 3) == 0 || (i % 5) == 0)
+sum = sum + i;
+}
+printf("%d\n", sum);
+
+return (0);
+}
