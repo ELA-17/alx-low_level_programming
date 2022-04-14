15b11820460e78823a116ba38e6c710051e39c54
elyas haile <eloyee7@gmail.com>
2022-04-14 05:49:19 -0700

    pr nm 777

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,21 @@
 
 /**
  * print_numbers - print number from zero to nine
+ *
+ * Return: return zero
  */
-
 void print_numbers(void)
 {
-int i,
-for (i = 48; i <= 57; i++)
+
+int j,
+
+for (j = 48; j <= 57; j++)
 {
-_putchar(i);
+
+_putchar(j);
+
 }
+
 _putchar('\n');
+
 }
